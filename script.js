648065861224780ae79e22be131912056ee5668f
Aigengrau <ochilovanis@gmail.com>
2023-01-30 18:10:11 +0300

    deselect

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const cards = document.querySelectorAll('.card');
-
+cards.forEach(card => {
+  if (card.dataset.soldOut === "true") {
+    card.classList.add("disabled");
+    
+  }
+});
 for (const card of cards) {
   card.addEventListener('click', function () {
     this.classList.toggle('selected');
